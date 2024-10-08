ee270191f1be33cf37542fc8c4cb588db97324b5
bohenriksen <bo@bohenriksen.com>
2024-10-08 13:40:57 +0200

    added chi

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"github.com/go-chi/chi/v5"
+	
+)
+
+func main() {
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+
+	}
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error: ", err)
+	}
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
+}
